5b0bd5cbffd24e781e07eae65ca84d2768b024c1
Nick Semerad <nicksemerad@gmail.com>
2020-03-05 19:59:49 -0700

    appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,55 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @appointments = Appointments.all
   end
 
   def show
   end
 
   def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to @appointment
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to @appointment
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(
+      :location, 
+      :time, 
+      :doctor_id, 
+      :patient_id
+      )
+  end
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root 'appointment#index'
+root 'appointments#index'
 
   resources :patients do
     resources :appointments
